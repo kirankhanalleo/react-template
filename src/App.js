cdb4194ada37eede3033316a4739aab67905dfee
kirankhanalleo <iamkirankhanal@gmail.com>
2024-01-19 18:50:40 +0545

    initial-commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello Kiran</h1>
+      <p>Welcome to react app</p>
+    </div>
+  );
+}
+
+export default App;
